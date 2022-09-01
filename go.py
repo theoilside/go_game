5f5832a77e5fd5e1f76e9d0b99b5cdd3fd79515d
Artemii Kurganov <aokurganov@mail.ru>
2022-09-01 16:01:03 +0500

    Make it work

--- go.py
+++ go.py
@@ -14,7 +14,7 @@ class Game:
         return ReqestResponse.StartGameResponse(True, Colors.black)
 
     def place_piece(self, x, y):
-        self.board.place_piece(x, y)
+        self.board.place_piece(self.color_of_current_move, x, y)
         self.color_of_current_move = self.color_of_current_move.get_opposite()
 
     def make_player_move(self, x, y):
@@ -29,7 +29,7 @@ class SingleplayerGame(Game):
         self.color_of_AI = None
         self.AI = None
 
-    def start_new_game(self, size, color_of_human: Colors = 'black'):
+    def start_new_game(self, size, color_of_human: Colors = Colors.black):
         self.color_of_human = color_of_human
         self.color_of_AI = color_of_human.get_opposite()
         self.AI = AI(self.board)
@@ -143,23 +143,24 @@ class Cell:
         self.type = type
 
 
-game = SingleplayerGame()
-print('Напиши, каким цветом хочешь играть — «b» или «w». Первыми ходят черные')
-inputted_color = input()
-if inputted_color == 'w':
-    color_of_human = Colors.white
-elif inputted_color == 'b':
-    color_of_human = Colors.black
-else:
-    raise 'Неправильный цвет!'
-print('Напиши размер поля')
-inputted_size = input()
-if int(inputted_size) < 2:
-    raise 'Неправильный размер!'
-game.start_new_game(int(inputted_size), color_of_human)
-while True:
-    print(game.board)
-    print('Напиши свой ход в формате «x,y», где «x» и «y» — координаты')
-    inputted_coords = input().split(',')
-    game.place_piece(int(inputted_coords[0]), int(inputted_coords[1]))
-    game.make_ai_move()
+if __name__ == '__main__':
+    game = SingleplayerGame()
+    print('Напиши, каким цветом хочешь играть — «b» или «w». Первыми ходят черные')
+    inputted_color = input()
+    if inputted_color == 'w':
+        color_of_human = Colors.white
+    elif inputted_color == 'b':
+        color_of_human = Colors.black
+    else:
+        raise 'Неправильный цвет!'
+    print('Напиши размер поля')
+    inputted_size = input()
+    if int(inputted_size) < 2:
+        raise 'Неправильный размер!'
+    game.start_new_game(int(inputted_size), color_of_human)
+    while True:
+        print(game.board)
+        print('Напиши свой ход в формате «x,y», где «x» и «y» — координаты')
+        inputted_coords = input().split(',')
+        game.place_piece(int(inputted_coords[0]), int(inputted_coords[1]))
+        game.make_ai_move()
