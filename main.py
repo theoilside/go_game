3fcde9621f0be41da4269d86bafc49dbe658c832
Artemii Kurganov <aokurganov@mail.ru>
2022-10-16 14:59:59 +0500

    Add config.json

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import logging
 import datetime
 import os
+import json
 
 from game.display import start_gui
 from game.cli import start_cli
 
+
+class Config:
+    def __init__(self, open_gui_default: bool, enable_logging: bool):
+        self.open_gui_default = open_gui_default
+        self.enable_logging = enable_logging
+
+
 PATH_TO_SAVE_LOGS = './logs'
 if __name__ == '__main__':
-    if not os.path.exists(PATH_TO_SAVE_LOGS):
-        os.makedirs(PATH_TO_SAVE_LOGS)
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
-                        datefmt='%d %b %Y %H:%M:%S',
-                        filename=f'logs/{datetime.datetime.now().strftime("%Y%m%d %H%M%S")}.log', filemode='w')
+    # Load config
+    with open('config.json', 'r') as f:
+        config_dict = json.loads(f.read())
+        config = Config(**config_dict)
+
+    if config.enable_logging:
+        if not os.path.exists(PATH_TO_SAVE_LOGS):
+            os.makedirs(PATH_TO_SAVE_LOGS)
+        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%d %b %Y %H:%M:%S',
+                            filename=f'logs/{datetime.datetime.now().strftime("%Y%m%d %H%M%S")}.log', filemode='w')
+
+    if config.open_gui_default:
+        start_gui()
+        exit(1)
+
     print('Какую версию запустить?')
     print('[графическая — 0 (default); консольная — 1]')
     print('Ввод:', end=' ')
@@ -24,4 +43,3 @@ if __name__ == '__main__':
         start_cli()
     else:
         raise NameError(f'Неверный ввод. Получено: {interface}. Допустимо: 0, 1')
-
