330e67309a40a052ea17362d5e9853330a4b45f6
Artemii Kurganov <aokurganov@mail.ru>
2022-10-16 12:25:40 +0500

    Fix issue when exit game

--- game/display.py
+++ game/display.py
@@ -19,9 +19,6 @@ class Display:
         self.image_storage = ImageStorage()
         self.element_creator = ElementCreator()
 
-        self.is_game_active = False
-        self.leaderboard: List[Tuple[str, int]] = []
-
         window.geometry(f'{WIDTH}x{HEIGHT}')
         window.title('Игра "Го"')
         window.iconbitmap(ICON_PATH)
@@ -29,105 +26,18 @@ class Display:
         self.create_frames()
 
     def create_frames(self):
+        self.frame_storage.configure_frames(self.element_creator, self.on_leaderboard_open, self.on_chosen_player_count,
+                                            self.on_chosen_field_size, self.on_exit_game_by_user)
         self.frame_storage.menu_frame.pack()
 
-        # Main frame config
-        self.element_creator.create_label('Игра Го', self.frame_storage.menu_frame)
-        self.element_creator.create_button('Начать игру', self.frame_storage.menu_frame,
-                                           callback=lambda:
-                                           self.change_frame(self.frame_storage.menu_frame,
-                                                             self.frame_storage.game_players_count_frame))
-        self.element_creator.create_button('Лидерборд', self.frame_storage.menu_frame,
-                                           callback=self.on_leaderboard_open)
-        self.element_creator.create_button('Правила игры', self.frame_storage.menu_frame,
-                                           callback=lambda: self.change_frame(self.frame_storage.menu_frame,
-                                                                              self.frame_storage.rule_frame))
-
-        self.element_creator.create_button('Выход', self.frame_storage.menu_frame, callback=lambda: self.window.quit())
-
-        # Frame with game type config
-        self.element_creator.create_label('Выберете режим игры', self.frame_storage.game_players_count_frame, width=20)
-        self.element_creator.create_button('С компьютером', self.frame_storage.game_players_count_frame,
-                                           callback=lambda: self.on_chosen_player_count(TypesOfGames.singleplayer))
-        self.element_creator.create_button('Два игрока', self.frame_storage.game_players_count_frame,
-                                           callback=lambda: self.on_chosen_player_count(TypesOfGames.multiplayer))
-        self.element_creator.create_button('Назад', self.frame_storage.game_players_count_frame,
-                                           callback=lambda: self.change_frame(
-                                               self.frame_storage.game_players_count_frame,
-                                               self.frame_storage.menu_frame))
-
-        # Frame with field size config
-        self.element_creator.create_label('Выберете размер игрового поля', self.frame_storage.game_size_frame, width=20)
-        self.element_creator.create_button('9x9', self.frame_storage.game_size_frame, width=10,
-                                           callback=lambda: self.on_chosen_field_size(9))
-        self.element_creator.create_button('13x13', self.frame_storage.game_size_frame, width=10,
-                                           callback=lambda: self.on_chosen_field_size(13))
-        self.element_creator.create_button('19x19', self.frame_storage.game_size_frame, width=10,
-                                           callback=lambda: self.on_chosen_field_size(19))
-        self.element_creator.create_button('Назад', self.frame_storage.game_size_frame, width=10,
-                                           callback=lambda: self.change_frame(
-                                               self.frame_storage.game_size_frame,
-                                               self.frame_storage.game_players_count_frame))
-
-        # Frame with rules config
-        self.element_creator.create_label('Правила игры', self.frame_storage.rule_frame, width=20)
-
-        text = ' ' * 4 + f'\n\n{" " * 4}'.join(RULES)
-
-        rule_label = self.element_creator.create_label(text, self.frame_storage.rule_frame, width=60,
-                                                       font=CALIBRI_SMALL_FONT,
-                                                       justify='left')
-        self.element_creator.create_button('Назад', self.frame_storage.rule_frame, width=10,
-                                           callback=lambda: self.change_frame(self.frame_storage.rule_frame,
-                                                                              self.frame_storage.menu_frame))
-
-        scroll = tk.Scrollbar(self.frame_storage.rule_frame, orient=tk.VERTICAL)
-        scroll.pack(side=tk.RIGHT, fill=tk.Y)
-        scroll.config(command=rule_label)
-
-        # Frame with leaderboard config
-        if len(self.leaderboard) == 0:
-            self.element_creator.create_label('Таблица лидеров пуста', self.frame_storage.leaderboard_frame, width=20)
-        else:
-            self.element_creator.create_label('Таблица лидеров', self.frame_storage.leaderboard_frame, width=20)
-            for (name, score) in self.leaderboard:
-                self.element_creator.create_label(f'{name} : {score}', self.frame_storage.leaderboard_frame, width=30)
-
-        self.element_creator.create_button('Назад', self.frame_storage.leaderboard_frame, width=10,
-                                           callback=lambda: self.change_frame(self.frame_storage.leaderboard_frame,
-                                                                              self.frame_storage.menu_frame))
-
-        # Frame with escape items config
-        self.window.bind('<Escape>', lambda e: self.change_frame(self.frame_storage.game_frame,
-                                                                 self.frame_storage.escape_frame)
-        if self.is_game_active else None)
-
-        self.element_creator.create_button('Продолжить игру', self.frame_storage.escape_frame, width=20,
-                                           callback=lambda: self.change_frame(self.frame_storage.escape_frame,
-                                                                              self.frame_storage.game_frame))
-
-        self.element_creator.create_button('Выйти в главное меню', self.frame_storage.escape_frame, width=20,
-                                           callback=lambda: _exit_game_by_user())
-
-        def _exit_game_by_user():
-            self.change_frame(self.frame_storage.escape_frame,
-                              self.frame_storage.menu_frame)
-            self.game_settings = GameSettings()
-            self.is_game_active = False
-
     def on_leaderboard_open(self):
-        self.leaderboard = self.game_settings.game_state.get_leaderboard().leaderboard
-        self.change_frame(self.frame_storage.menu_frame,
-                          self.frame_storage.leaderboard_frame)
-
-    @staticmethod
-    def change_frame(old_frame, new_frame):
-        old_frame.pack_forget()
-        new_frame.pack()
+        self.frame_storage.leaderboard = self.game_settings.game_state.get_leaderboard().leaderboard
+        self.frame_storage.change_frame(self.frame_storage.menu_frame,
+                                        self.frame_storage.leaderboard_frame)
 
     def on_chosen_player_count(self, game_type: TypesOfGames):
         self.game_settings.game_type = game_type
-        self.change_frame(self.frame_storage.game_players_count_frame, self.frame_storage.game_size_frame)
+        self.frame_storage.change_frame(self.frame_storage.game_players_count_frame, self.frame_storage.game_size_frame)
 
     def on_chosen_field_size(self, size):
         self.game_settings.size = size
@@ -135,8 +45,16 @@ class Display:
         start_game_response: StartGameResponse = self.game_settings.game_state.start_new_game(size)
         self.game_settings.current_color = start_game_response.current_turn
         self.init_game_field()
-        self.change_frame(self.frame_storage.game_size_frame, self.frame_storage.game_frame)
-        self.is_game_active = True
+        self.frame_storage.change_frame(self.frame_storage.game_size_frame, self.frame_storage.game_frame)
+        self.frame_storage.is_game_active = True
+
+    def on_exit_game_by_user(self):
+        old_frame = self.frame_storage.escape_frame
+        self.game_settings = GameSettings()
+        self.frame_storage = FrameStorage(self.window)
+        self.create_frames()
+        self.frame_storage.change_frame(old_frame, self.frame_storage.menu_frame)
+        self.frame_storage.is_game_active = False
 
     def init_game_field(self):
         self.game_settings.init_game_state(self.frame_storage.game_frame, self.on_game_cell_pressed)
